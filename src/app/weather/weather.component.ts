c14cb43fe970e607f4f01c5966d8c5e70e4140b1
Eimantas <eim.kasp@gmail.com>
2022-08-17 17:57:11 +0300

    Bazinis http užklausos pavyzdys

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  // Po : aprašomas kintamojo tipas
+  weatherData: any;
+  city: string = "Vilnius";
+  apik: string = "3045dd712ffe6e702e3245525ac7fa38";
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    // Iškivetus komponenta, išškviečiame duomenų gavimo funkcija
+    this.getWeatherData();
+  }
+
+  getWeatherData() {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      this.city +
+      "&appid=" +
+      this.apik
+    )
+    .then((res) => res.json())
+    .then((data) => {
+      this.weatherData = data;
+      console.log(this.weatherData);
+    })
+    .catch((err) => alert("You entered WRONG city name !!!"));
   }
 
 }
